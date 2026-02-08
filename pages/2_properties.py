3017f78c7c5a68c42334c6c6cbc8ca7a27a22608
Harmony <mikaelabrobinson@gmail.com>
2026-02-08 17:24:11 -0500

    Load data and initialize properties in 2_properties.py

--- pages/2_properties.py
+++ pages/2_properties.py
@@ -1,2 +1,7 @@
 from data_access import load_data
 
+data = load_data()
+items = data.get("items", []) or []
+criteria = data.get("criteria", {}) or {}
+last_updated = data.get("last_updated_utc")
+
